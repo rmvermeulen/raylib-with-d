138bfabf8a43836ecd2c22d4cf63dcc77106911e
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-07-26 20:16:03 +0200

    Hello raylib and D!

---
+++ source/app.d
@@ -0,0 +1,15 @@
+import std.stdio;
+import raylib;
+
+void main()
+{
+	InitWindow(800, 600, "Hello, Raylib-D!");
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Hello, World!", 400, 300, 28, BLACK);
+		EndDrawing();
+	}
+	CloseWindow();
+}
