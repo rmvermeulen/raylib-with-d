944bd32c4f286f21a80d52e6c467ab4a1531cc95
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-07-31 02:44:42 +0200

    some code

--- source/app.d
+++ source/app.d
@@ -1,14 +1,43 @@
 import std.stdio;
 import raylib;
 
+/// nothing yet
+class GameObject
+{
+	/// world position
+	Vector2 position;
+	/// called at the appropriate time
+	void onDraw()
+	{
+	}
+}
+
+/// Simple label
+class Label : GameObject
+{
+	/// What the label displays
+	string text = "This is a label";
+	override void onDraw()
+	{
+		DrawText(text.ptr, pos.x, pos.y, 28, BLACK);
+	}
+}
+
 void main()
 {
+	GameObject[] entities = [
+		new Label(Vector2(400, 300), "This is a label")
+	];
+
 	InitWindow(800, 600, "Hello, Raylib-D!");
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText("Hello, World!", 400, 300, 28, BLACK);
+		foreach (entity; entities)
+		{
+			entity.onDraw();
+		}
 		EndDrawing();
 	}
 	CloseWindow();
